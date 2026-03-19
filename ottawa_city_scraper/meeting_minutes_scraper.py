dbfe6ae3a567636f735af31264dfbe5a3dba1031
Sonal Ranjit <sonal.ranjit3@gmail.com>
2026-03-19 16:21:52 -0400

    Parsed agenda header

--- ottawa_city_scraper/meeting_minutes_scraper.py
+++ ottawa_city_scraper/meeting_minutes_scraper.py
@@ -21,16 +21,15 @@ def load_html_file(file_path: str | Path) -> str:
 
 def normalize_councillor_name(raw_name: str) -> str:
     name = raw_name.strip().rstrip(",")
-    return name.removeprefix("Mayor ").removeprefix("Councillor ")
+    return name.removeprefix("Mayor ").removeprefix("and ").removeprefix("Councillor ")
 
-def parse_minutes_html(html: str, source: str) -> dict:
-    soup = BeautifulSoup(html, "html.parser")
-    agenda_header_details_table = soup.find('div', class_='AgendaHeaderDetailsTable')
+def parse_header(agenda_header: BeautifulSoup) -> dict:
+    agenda_header_details_table = agenda_header.find('div', class_='AgendaHeaderDetailsTable')
     meeting_number = agenda_header_details_table.find('div', class_='AgendaMeetingNumberText').get_text(strip=True)
     meeting_date = agenda_header_details_table.find('div', class_='AgendaMeetingTime').find('time')['datetime']
     meeting_start_time = agenda_header_details_table.find('span', class_='AgendaMeetingTimeStart').find('time')['datetime']
     meeting_location = agenda_header_details_table.find('div', class_='Location').get_text(strip=True)
-    agenda_header_attendance_table = soup.find('div', class_='AgendaHeaderAttendanceTable').find_all('div')
+    agenda_header_attendance_table = agenda_header.find('div', class_='AgendaHeaderAttendanceTable').find_all('div')
     present_attendees = []
     absent_attendees = []
 
@@ -46,16 +45,30 @@ def parse_minutes_html(html: str, source: str) -> dict:
             for councillor in absent_councillors:
                 logger.info("Absent councillor: %s", normalize_councillor_name(councillor.get_text(strip=True, separator=" ")))
                 absent_attendees.append(normalize_councillor_name(councillor.get_text(strip=True, separator=" ")))
-
     return {
-        "source": source,
-        "title": soup.title.get_text(strip=True) if soup.title else None,
         "meeting_number": int(meeting_number),
         "meeting_date": meeting_date,
         "meeting_start_time": meeting_start_time,
         "meeting_location": meeting_location,
         "present_attendees": present_attendees,
-        "absent_attendees": absent_attendees,
+        "absent_attendees": absent_attendees
+    }
+
+def parse_minutes_html(html: str, source: str) -> dict:
+    soup = BeautifulSoup(html, "html.parser")
+    agenda_header = soup.find('header', class_='AgendaHeader')
+    logger.info(agenda_header)
+    parsed_header = parse_header(agenda_header)
+
+    return {
+        "source": source,
+        "title": soup.title.get_text(strip=True) if soup.title else None,
+        "meeting_number": parsed_header["meeting_number"],
+        "meeting_date": parsed_header["meeting_date"],
+        "meeting_start_time": parsed_header["meeting_start_time"],
+        "meeting_location": parsed_header["meeting_location"],
+        "present_attendees": parsed_header["present_attendees"],
+        "absent_attendees": parsed_header["absent_attendees"],
         "motions": [],
         "votes": [],
     }
